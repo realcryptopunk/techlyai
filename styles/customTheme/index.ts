5571bb30418bd9c866b719c94d93cc95ffd8672a
realcryptopunk <nrohan1@fordham.edu>
2023-03-03 04:21:07 -0500

    index fix

---
+++ styles/customTheme/index.ts
@@ -0,0 +1,13 @@
+export const blogDashboard = {
+    shadow: "lg",
+    padding: "15px",
+    borderRadius: "10px",
+    borderColor: "gray.200",
+    borderWidth: "1px",
+    cursor: "pointer",
+    _hover: {
+      boxShadow: "xl",
+      borderColor: "gray.430",
+      transitionDuration: "200ms",
+    },
+  };
